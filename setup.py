24f0dcdee2134bc940a83bbf1256eca2b4accdf5
kavorite <iseurie@gmail.com>
2022-01-21 20:17:17 -0500

    egg info

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="sharpness-aware-minimization",
+    version="1.0",
+    description="Sharpness-aware minimization in JAX",
+    author="kavorite",
+    author_email="kavorite@gmail.com",
+    url="https://github.com/kavorite/sam",
+)
