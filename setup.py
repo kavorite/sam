a316c0836a71b319ebe2b31505e845f59efb42ad
æriess <iseurie@gmail.com>
2022-08-10 04:25:00 -0400

    proper versioning

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import find_namespace_packages, setup
 
 setup(
     name="sam",
-    version="1.0.2",
+    version="0.0.2",
     description="Sharpness-aware minimization in optax",
     author="kavorite",
     url="https://github.com/kavorite/sam",
-    install_requires=["dm_haiku>=0.0.5" "jax>=0.2.27" "optax>=0.0.9"],
+    install_requires=["dm_haiku>=0.0.5" "jax>=0.2" "optax>=0.1"],
     packages=find_namespace_packages(),
 )
